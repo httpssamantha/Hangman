a6718b4e017840aea75579a7573fef02da7345f4
samantha <62360959+httpssamantha@users.noreply.github.com>
2020-04-12 15:20:09 -0400

    Add files via upload

--- hangman_app.py
+++ hangman_app.py
@@ -3,13 +3,87 @@
    it is also used as a module in the hangman_webapp flask app
 """
 
-def generate_random_word():
-   """ read a list of words from a file and pick a random one to return """
-   return "unimplemented"
 
 def play_hangman():
-   """ this is the python script version of the game """
-   print("The hangman app is under construction. Try again later!")
+    import csv
+    import random
+    cool = []
+    f = open('words 3.csv')
+    words = csv.reader(f)
+    for row in words:
+        cool.append(row)
+    cool.append(words)
+    theword = str(random.choice(cool))
+    guess = theword[2:][:-2]
+    unique = ''.join(set(guess))
+    check = list(unique)
+    print(guess)
+    print(len(guess))
+    counter=len(unique)
+
+    want_to_play = True
+
+
+    while (want_to_play):
+        guessed_lettersright= []
+        rightcounter=0
+        guessed_letterswrong= ["-","-","-","-","-","-"]
+        guesses_left = 10
+        word = guess
+        letter = input("Please input a letter >")
+        done = False
+        blanks = "- "*len(guess)
+        print(blanks)
+        while not done:
+            if letter in guessed_letterswrong or letter in guessed_lettersright:
+                guesses_left-=1
+                print("You have already guessed that letter, guess another one!")
+              
+            elif letter not in word:
+                guessed_letterswrong.remove("-")
+                guessed_letterswrong.append(letter)
+                print("This letter is not in the word, guess again.")
+                guesses_left-=1
+                #add letter to guessed letters"
+                #tell user the letter is not in the word"
+                #subtract one from the guesses_left"
+            else:
+                guessed_lettersright.append(letter)
+                guesses_left-=1
+                rightcounter+=1
+                print("This letter is in the word!")
+                #add letter to guessed letters"
+                #tell user the letter is in the word"
+    
+            if guesses_left==0:
+                print("The number of guesses left is zero, You lose!")
+                done=True
+            #set done to be true and tell the user they lost!"
+            if counter==rightcounter:
+                print("You got the word correct! It was",guess+ ". You win!")
+                done=True
+                break
+            else:
+                print(guessed_letterswrong)
+               
+                #print the word with a dash for each letter not in guessed_letters"
+                letter = input("Type in another letter")
+            def is_letter_in_word(check,guesses):
+                for letter in check:
+                    if letter not in check:
+                        print("this letter is in the word")
+                        return False
+                return True
+            #def is_guess_correct(check, guesses):
+                #for let
+            
+                #all the letters in the word have been guessed"
+                #set done to be true and tell the user they won!"
+            is_letter_in_word(check, guessed_lettersright)
+        want_to_play = input("Do your want to play Hangman?\nType 'done' to quit: \n")
+        if want_to_play == "Done" or want_to_play == "done":
+            done = True
+            break
 
 if __name__ == '__main__':
     play_hangman()
